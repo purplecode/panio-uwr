a002b46d6210803d26a6211b82d7565cc5a1d59f
unknown <piotr@waszkiewicz.eu>
2013-03-20 17:03:41 +0100

    FunctionInput class

---
+++ src/com/nsn/uwr/panio/inputsparser/ArgumentsParser.java
@@ -0,0 +1,19 @@
+package com.nsn.uwr.panio.inputsparser;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ArgumentsParser {
+	
+	public FunctionInput parse(String[] args) {
+		
+		FunctionInput fu= new FunctionInput();
+		fu.setArg1(Double.parseDouble(args[0]));
+		fu.setArg2(Dou
+		
+		
+		
+		throw new NotImplementedException();
+
+	}
+
+}
---
+++ test/com/nsn/uwr/panio/inputsparser/ArgumentsParserTest.java
@@ -0,0 +1,31 @@
+package com.nsn.uwr.panio.inputsparser;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArgumentsParserTest {
+
+	private ArgumentsParser argumentsParser;
+
+	@Before
+	public void setUp() {
+		argumentsParser = new ArgumentsParser();
+	}
+
+	@Test
+	public void testSimpleArgumentsParsing() {
+		// given
+		String[] input = { "1.5", "2.5", "+" };
+
+		// when
+		FunctionInput result = argumentsParser.parse(input);
+
+		// then
+		Assert.assertEquals(1.5, result.getArg1());
+		Assert.assertEquals(2.5, result.getArg2());
+		Assert.assertEquals('+', result.getOperand());
+	}
+
+}
