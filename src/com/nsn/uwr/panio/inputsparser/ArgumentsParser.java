a0713f24b14bd0c47a9fae25d46082794d3446a8
unknown <piotr@waszkiewicz.eu>
2013-03-20 17:17:58 +0100

    Parser now passing simple test

--- src/com/nsn/uwr/panio/inputsparser/ArgumentsParser.java
+++ src/com/nsn/uwr/panio/inputsparser/ArgumentsParser.java
@@ -1,19 +1,18 @@
 package com.nsn.uwr.panio.inputsparser;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class ArgumentsParser {
-	
-	public FunctionInput parse(String[] args) {
-		
-		FunctionInput fu= new FunctionInput();
-		fu.setArg1(Double.parseDouble(args[0]));
-		fu.setArg2(Dou
-		
-		
-		
-		throw new NotImplementedException();
 
+	private double parseNumericArgument(String arg) {
+		return Double.parseDouble(arg);
+	}
+
+	public FunctionInput parse(String[] args) {
+		FunctionInput fu = new FunctionInput();
+		fu.setArg1(parseNumericArgument(args[0]));
+		fu.setArg2(parseNumericArgument(args[1]));
+		fu.setOperand(args[2].charAt(0));
+		return fu;
 	}
 
 }
