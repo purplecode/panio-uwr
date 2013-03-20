770965c2c71488cc639ece859d1e18f6f458e71e
unknown <piotr@waszkiewicz.eu>
2013-03-20 17:52:53 +0100

    Test now working

--- src/com/nsn/uwr/panio/Logger.java
+++ src/com/nsn/uwr/panio/Logger.java
@@ -3,18 +3,17 @@ package com.nsn.uwr.panio;
 import java.text.DateFormat;
 
 public class Logger {
-	
-	public void loggMessage(ELoggLevel loglevel, String message  ){
-		
+
+	public void loggMessage(ELoggLevel loglevel, String message) {
+
 		DateFormat datetimeFormatter = DateFormat.getDateTimeInstance();
-		
+
 		java.util.Date date = new java.util.Date();
 		String dateString = datetimeFormatter.format(date);
-		
-		System.out.println("[] "+ dateString+" [" +loglevel+  "] "+message);
-		
-		
+
+		System.out.println("[" + dateString + "]" + " [" + loglevel + "] "
+				+ message);
+
 	};
-		
 
 }
--- test/com/nsn/uwr/panio/LoggerTest.java
+++ test/com/nsn/uwr/panio/LoggerTest.java
@@ -3,6 +3,9 @@ package com.nsn.uwr.panio;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import junit.framework.Assert;
+
+import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,10 +22,19 @@ public class LoggerTest {
 	};
 
 	@Test
-	public void shouldPrintSomething() {
+	public void shouldPrintInfo() {
+		// given
 		Logger loggerUnderTest = new Logger();
-		loggerUnderTest.loggMessage(ELoggLevel.INFO, "My message");
+		ELoggLevel loglevel = ELoggLevel.INFO;
+		String message = "My message";
 
+		// when
+		loggerUnderTest.loggMessage(loglevel, message);
+
+		// then
+		String trimedlog = byteArrayOutputStream.toString().trim();
+		Assertions.assertThat(trimedlog).endsWith(message);
+		Assertions.assertThat(trimedlog).contains(loglevel.toString());
 	};
 
 }
