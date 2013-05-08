d51e444d9715a1a3815d50353d5970cfb69147cc
wiktorch <wiktus.1990@gmail.com>
2013-05-08 16:37:49 +0200

    console call arguments are interpreted.

--- src/com/nsn/uwr/panio/Calculator.java
+++ src/com/nsn/uwr/panio/Calculator.java
@@ -1,8 +1,18 @@
 package com.nsn.uwr.panio;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.nsn.uwr.panio.calculations.AlgebraicCalculations;
 import com.nsn.uwr.panio.inputsparser.ArgumentsParser;
 import com.nsn.uwr.panio.inputsparser.FunctionInput;
+import com.nsn.uwr.panio.logger.Logger;
 import com.nsn.uwr.panio.outputhandler.OutputHandler;
 
 /**
@@ -12,20 +22,54 @@ import com.nsn.uwr.panio.outputhandler.OutputHandler;
  */
 public class Calculator {
 
+	private static final char INLINE_ARITHMETIC_OPERATION = 'l';
+	private static final char OUTPUT_FILE_OPTION = 'o';
+	private static final char INPUT_FILE_OPTION = 'f';
+
 	/**
 	 * @param args
 	 */
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
+		Options cmdOptions = new Options();
+		CommandLineParser parser = new BasicParser();
+		Option lOption = OptionBuilder.hasArg(true).withArgName(String.valueOf(INLINE_ARITHMETIC_OPERATION)).withDescription("wyrazenie arytmetyczne").create();
+		Option fOption = OptionBuilder.hasArg(true).withArgName(String.valueOf(INPUT_FILE_OPTION)).withDescription("plik wejsciowy do przetwarzania wsadowego").create();
+		Option oOption = OptionBuilder.hasArg(true).withArgName(String.valueOf(OUTPUT_FILE_OPTION)).withDescription("nazwa pliku wyjsciowego").create();
 		
-		ArgumentsValidator argumentsValidator = new ArgumentsValidator();
-		argumentsValidator.validateArguments(args);
+		OptionGroup mutuallyExclusiveOptions = new OptionGroup();
+
+		mutuallyExclusiveOptions.addOption(lOption).addOption(fOption).setRequired(true);
 		
-		ArgumentsParser argumentsParser = new ArgumentsParser();
-		FunctionInput functionInput = argumentsParser.parse(args);
-		AlgebraicCalculations algebraicCalculations = new AlgebraicCalculations();
-		double result = algebraicCalculations.invoke(functionInput);
-		OutputHandler outputHandler = new OutputHandler();
-		outputHandler.resultToStdOut( result );
+		cmdOptions.addOptionGroup(mutuallyExclusiveOptions).addOption(oOption);
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(cmdOptions, args);
+		} catch (ParseException e) {
+			Logger.getInstance().logError(e.getMessage());
+			return;
+		}
+		
+		if(cmd.hasOption(OUTPUT_FILE_OPTION)) {
+			String outputFilename = cmd.getOptionValue('o');
+			// TODO file output handler creation
+		}
+		if(cmd.hasOption(INLINE_ARITHMETIC_OPERATION)) {
+			ArgumentsValidator argumentsValidator = new ArgumentsValidator();
+			argumentsValidator.validateArguments(cmd.getOptionValue('l').split(" "));
+			
+			ArgumentsParser argumentsParser = new ArgumentsParser();
+			FunctionInput functionInput = argumentsParser.parse(args);
+			AlgebraicCalculations algebraicCalculations = new AlgebraicCalculations();
+			double result = algebraicCalculations.invoke(functionInput);
+			OutputHandler outputHandler = new OutputHandler();
+			outputHandler.resultToStdOut( result );
+		}
+		if(cmd.hasOption(INPUT_FILE_OPTION)) {
+			String inputFilename = cmd.getOptionValue('o');
+			// TODO file output handler creation
+		}
+		
 	}
 
 }
